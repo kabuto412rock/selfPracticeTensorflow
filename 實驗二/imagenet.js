591658213c1c7e95ec80e7197d804c34ef755c8b
ZongKaKu <kabuto412rock@gmail.com>
2017-07-26 10:19:39 +0800

    TF的object_detection_第二個實驗_查詢圖片

---
+++ 實驗二/imagenet.js
@@ -0,0 +1,21 @@
+const download = require('image-downloader')
+var fs = require('fs')
+fs.readFile('ok_urls.json', 'utf8', function (err, data) {
+  var ok_urls = JSON.parse(data)
+  console.log('ok_urls:', ok_urls)
+
+  for (var i = 0; i < ok_urls.length; i++) {
+    options = {
+      url: ok_urls[i],
+      dest: 'dog' + i + '.jpg' // Save to /path/to/dest/photo.jpg 
+    }
+
+    download.image(options)
+      .then(({ filename, image }) => {
+        console.log('File saved to', filename)
+      }).catch((err) => {
+      errCount += 1
+      console.log('errCount = ', errCount)
+    })
+  }
+})
